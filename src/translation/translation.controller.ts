183efb6ef8abfbc22602ab3696ccc75600384af6
Kao Oat <tanawit139@gmail.com>
2021-03-23 21:37:02 +0700

    add get method for audio tag

--- src/translation/translation.controller.ts
+++ src/translation/translation.controller.ts
@@ -1,6 +1,18 @@
-import { Body, Controller, Header, HttpCode, Post, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Header,
+  HttpCode,
+  Post,
+  Query,
+  Res,
+} from '@nestjs/common';
 import { TranslationService } from './translation.service';
 import { TextToSpeechDto } from './translation.dto';
+import { google } from '@google-cloud/text-to-speech/build/protos/protos';
+import AudioEncoding = google.cloud.texttospeech.v1.AudioEncoding;
+import SsmlVoiceGender = google.cloud.texttospeech.v1.SsmlVoiceGender;
 
 @Controller('translation')
 export class TranslationController {
@@ -10,7 +22,26 @@ export class TranslationController {
   @HttpCode(200)
   @Header('Content-Type', 'audio/mpeg')
   // @Header('Content-Disposition', 'attachment; filename="hello-world.mp3"')
-  async testTTS(@Res() response, @Body() dto: TextToSpeechDto) {
+  async postTTS(@Res() response, @Body() dto: TextToSpeechDto) {
+    const readable = await this.translationService.textToSpeech(dto);
+    return readable.pipe(response);
+  }
+
+  @Get('/text-to-speech')
+  @Header('Content-Type', 'audio/mpeg')
+  // @Header('Content-Disposition', 'attachment; filename="hello-world.mp3"')
+  async getTTS(
+    @Res() response,
+    @Query('text') text: string,
+    @Query('lang') languageCode: string,
+    @Query('gender') gender: string,
+  ) {
+    const dto: TextToSpeechDto = {
+      text,
+      languageCode,
+      audioEncoding: AudioEncoding.MP3,
+      ssmlGender: SsmlVoiceGender[gender],
+    };
     const readable = await this.translationService.textToSpeech(dto);
     return readable.pipe(response);
   }
